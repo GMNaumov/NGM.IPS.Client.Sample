9d853f63011f787d8776024e0216303e569b8b76
Georgii Naumov <gmnaumov@yandex.ru>
2024-03-03 00:10:20 +0300

    feat: Полтора часа времени, и готова обработка выделенных объектов в Навигаторе

--- NGM.IPS.Client.AssignLetter/Program.cs
+++ NGM.IPS.Client.AssignLetter/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Intermech.Bars;
+using Intermech.DataFormats;
 using Intermech.Interfaces.Plugins;
+using Intermech.Navigator.ContextMenu;
+using Intermech.Navigator.SelectionView;
+using Intermech.Navigator.Interfaces;
 using System;
+using System.Windows.Forms;
+using Intermech.Search;
 
 namespace NGM.IPS.Client.AssignLetter
 {
@@ -10,6 +16,8 @@ namespace NGM.IPS.Client.AssignLetter
 
         private static IServiceProvider _ipsServiceProvider;
 
+        private static ISelectedItems _selectedItems;
+
         public void Load(IServiceProvider serviceProvider)
         {
             _ipsServiceProvider = serviceProvider;
@@ -17,7 +25,7 @@ namespace NGM.IPS.Client.AssignLetter
             IPluginManager plugins = (IPluginManager)_ipsServiceProvider.GetService(typeof(IPluginManager));
 
             // Получаем ссылку на сервис управления главным меню IPS
-            BarManager barManager = _ipsServiceProvider.GetService(typeof (BarManager)) as BarManager;
+            BarManager barManager = _ipsServiceProvider.GetService(typeof(BarManager)) as BarManager;
 
             // Получаем ссылку на главное меню IPS
             MenuBar menuBar = barManager.MenuBar;
@@ -35,15 +43,49 @@ namespace NGM.IPS.Client.AssignLetter
             menuButton.CommandName = "AssignLetterButton";
 
             menuButton.ToolTipText = "Присвоить литеру на основании документа-решения";
-            
+
+            menuButton.Click += LetterAssign;
+
             menuBar.Items.Add(menuItem);
-            
+
             menuItem.Items.Add(menuButton);
+        }
+
+        private void LetterAssign(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем объекты, выделенные пользователем в навигаторе IPS
+                _selectedItems = SelectedItemsHelper.GetNavigatorSelection();
+
+                int count = _selectedItems.Count;
+
+                if (count > 10)
+                {
+                    MessageBox.Show("Выделено слишком много объектов");
+                }
+                else
+                {
+                    MessageBox.Show($"Выделено {count} объектов");
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        // Попытка получить описание следующего выделенного объекта из коллекции
+                        IDBTypedObjectID objectID = _selectedItems.GetItemData(i, typeof(IDBTypedObjectID)) as IDBTypedObjectID;
+
+                        MessageBox.Show($"objectID.ID: {objectID.ID} || objectID.ObjectType: {objectID.ObjectType} || objectID.ObjectID:  {objectID.ObjectID} || objectID.Owner:  {objectID.Owner} || objectID.Caption:  {objectID.Caption} || objectID.SiteID:  {objectID.SiteID}");
+                    }
+                }
+            }
+            catch (NullReferenceException exeption)
+            {
+                MessageBox.Show("Выделите объекты типа Сборочная единица или Деталь в Навигаторе IPS для назначения литеры");
+            }
         }
 
         public void Unload()
         {
-            
+
         }
     }
 }
